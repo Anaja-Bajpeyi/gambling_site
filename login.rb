cf9c06cb21326d2ca6e5f933f4882fed2d257239
anaja <abajpeyi21@gmail.com>
2020-11-11 21:00:31 -0800

    Update login.rb

--- login.rb
+++ login.rb
@@ -42,7 +42,7 @@ get '/login' do
     if session[:user]
         erb :welcome
     else
-	session[:message] = "Failed Login..."
+	"Failed Login..."
         erb :login
     end
 end
@@ -60,7 +60,7 @@ post '/login' do
         session[:id]=id.User_id
         erb :welcome
     else
-	session[:message] = "Failed Login..."
+	"Failed Login..."
         erb :login
     end
 	
@@ -75,7 +75,7 @@ post '/bet' do
       erb :welcome
     else
        session[:lost] += stake
-        erb :welcome
+       erb :welcome
     end
 end
 
